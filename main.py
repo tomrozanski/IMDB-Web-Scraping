4cb3bd6f274c549b40ac5461a6152a78baec3f56
Tom <tomrozanski@gmail.com>
2021-07-21 21:38:43 +0300

    [Change] Create parser for command line tool

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from ImdbSearchFetcher import ImdbSearchFetcher
 from MovieScraper import MovieScraper
 from MovieFileWriter import MovieFileWriter
+import argparse
+
+parser = argparse.ArgumentParser(description="tool that can execute queries for movie titles using imdb web site")
+parser.add_argument('-m', '--movie_title', type=str, metavar="", required=True, help='Name of the movie')
+parser.add_argument('-p', '--file_path', type=str, metavar="", required=True, help='Path of the file')
+args = parser.parse_args()
+
+
+def scrape_movie(search):
+    fetcher = ImdbSearchFetcher()
+    scraper = MovieScraper()
+
+    filtered_movies = fetcher.filtering_movies(search)
+    scraped_movies = scraper.scrape(filtered_movies)
+    return scraped_movies
+
+
+def write_to_file(movies_to_write, path):
+    file_writer = MovieFileWriter()
+    file_writer.write_to_file(movies_to_write, path)
 
 
 if __name__ == '__main__':
-
-    fetcher = ImdbSearchFetcher()
-    scraper = MovieScraper()
-    file_writer = MovieFileWriter()
-
-    search_terms = input("What movie do you want to know about?\n> ")
-    filtered_movies = fetcher.filtering_movies(search_terms)
-    scraped_movies = scraper.scrape(filtered_movies)
-    file_writer.write_to_file(scraped_movies)
+    movies = scrape_movie(args.movie_title)
+    write_to_file(movies, args.file_path)
+    print("The scraping was done successfully")
