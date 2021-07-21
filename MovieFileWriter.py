2e6775365506079db4f9588d1cb292697c4cf69f
Tom <tomrozanski@gmail.com>
2021-07-21 21:37:36 +0300

    [Change] Get path from user

--- MovieFileWriter.py
+++ MovieFileWriter.py
@@ -1,9 +1,13 @@
+import os
 class MovieFileWriter(object):
 
-    def write_to_file(self, movies):
-        text_file = open("movies_file.txt", "w")
+    def write_to_file(self, movies, path):
+        save_path = path
+        file_name = "movies_file.txt"
+        final_path = os.path.join(save_path, file_name)
+        text_file = open(final_path, "w")
 
         for movie in movies:
             text_file.write(str(movie))
-
+        print("The movie text file is in: "+path)
         text_file.close()
